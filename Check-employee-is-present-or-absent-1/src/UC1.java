c26fa8e0b16617c32e0449bee65772a281d1e3a2
PavanKumar107 <99305975+PavanKumar107@users.noreply.github.com>
2022-03-10 13:42:06 +0530

    UC1

---
+++ Check-employee-is-present-or-absent-1/src/UC1.java
@@ -0,0 +1,14 @@
+
+public class UC1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a = 1;
+         double empCheck = Math.floor(Math.random() * 10) % 2;
+         if (empCheck == a)
+              System.out.println("Employee is Present");
+         else
+              System.out.println("Employee is Absent");
+	}
+
+}
